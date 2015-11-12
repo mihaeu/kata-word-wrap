63ded2c7bae8faf746dbd435d672ff8c9ccafdb4
Michael Haeuslmann <haeuslmann@gmail.com>
2015-11-12 18:17:32 +0100

    Put everything into a single function because of the requirements ...

--- src/WordWrap.php
+++ src/WordWrap.php
@@ -11,45 +11,25 @@ class WordWrap
         $currentLine = 0;
         $lines[$currentLine] = '';
 
-        $words = self::splitWords($source);
+        $wordsAndDelimiters = preg_split('/(\s)/', $source, -1, PREG_SPLIT_DELIM_CAPTURE);
+        $words = array_filter($wordsAndDelimiters, function ($element) {
+            if ($element != ' ') {
+                return $element;
+            }
+        });
         foreach ($words as $word) {
-            if (self::wordIsLinefeed($word)) {
+            if (preg_match('/\s/', $word) === 1) {
                 ++$currentLine;
                 $lines[$currentLine] = '';
             }
-            else if (self::currentLineIsEmpty($lines[$currentLine])) {
+            else if (strlen($lines[$currentLine]) === 0) {
                 $lines[$currentLine] = $word;
-            } else if (self::nextWordCanFitLine($lines[$currentLine], $word, $columns)) {
+            } else if ((strlen($lines[$currentLine]) + strlen(' '.$word)) > $columns) {
                 $lines[++$currentLine] = $word;
             } else {
                 $lines[$currentLine] .= ' '.$word;
             }
         }
         return implode(PHP_EOL, $lines);
-    }
-
-    private static function splitWords(\string $source) : array
-    {
-        $wordsAndDelimiters = preg_split('/(\s)/', $source, -1, PREG_SPLIT_DELIM_CAPTURE);
-        return array_filter($wordsAndDelimiters, function ($element) {
-            if ($element != ' ') {
-                return $element;
-            }
-        });
-    }
-
-    private static function wordIsLineFeed(\string $word) : \bool
-    {
-        return preg_match('/\s/', $word) === 1;
-    }
-
-    private static function currentLineIsEmpty(\string $line) : \bool
-    {
-        return strlen($line) === 0;
-    }
-
-    private static function nextWordCanFitLine($line, $nextWord, $columns) : \bool
-    {
-        return (strlen($line) + strlen(' '.$nextWord)) > $columns;
     }
 }
