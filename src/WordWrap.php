d2c9759e854c99de1d51965bd3eaac1156d0c87d
Michael Haeuslmann <haeuslmann@gmail.com>
2015-11-12 17:29:31 +0100

    Made it work.

--- src/WordWrap.php
+++ src/WordWrap.php
@@ -1,11 +1,36 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Mihaeu\Kata;
 
 class WordWrap
 {
-    public static function wrap(\string $source, \int $columns)
+    public static function wrap(\string $source, \int $columns) : \string
     {
-        return $source;
+        /** @var string[] $lines */
+        $lines = [];
+        $currentLine = 0;
+        $lines[$currentLine] = '';
+
+        $words = explode(' ', $source);
+        foreach ($words as $word) {
+            if (self::currentLineIsEmpty($lines[$currentLine])) {
+                $lines[$currentLine] = $word;
+            } else if (self::nextWordCanFitLine($lines[$currentLine], $word, $columns)) {
+                $lines[++$currentLine] = $word;
+            } else {
+                $lines[$currentLine] .= ' '.$word;
+            }
+        }
+        return implode(PHP_EOL, $lines);
+    }
+
+    private static function currentLineIsEmpty(\string $line) : \bool
+    {
+        return strlen($line) === 0;
+    }
+
+    private static function nextWordCanFitLine($line, $nextWord, $columns) : \bool
+    {
+        return (strlen($line) + strlen(' '.$nextWord)) > $columns;
     }
 }
--- tests/WordWrapTest.php
+++ tests/WordWrapTest.php
@@ -1,11 +1,21 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Mihaeu\Kata;
 
 class WordWrapTest extends \PHPUnit_Framework_TestCase
 {
-    public function testWrap()
+    public function testFitsOneLongWordInOneColumn()
     {
-        $this->assertEquals('test', WordWrap::wrap('test', 10));
+        $this->assertEquals('123456789', WordWrap::wrap('123456789', 6));
+    }
+
+    public function testWrapsALineAfterTenColumns()
+    {
+        $this->assertEquals("test test\ntest", WordWrap::wrap('test test test', 10));
+    }
+
+    public function testWrapsWhenPossibleOtherwiseBreaksBoundaties()
+    {
+        $this->assertEquals("testtesttesttest\ntest", WordWrap::wrap('testtesttesttest test', 10));
     }
 }
