b01bbbc9fd7af1b51f9a56cd6c06d41fa705c206
Michael Haeuslmann <haeuslmann@gmail.com>
2015-11-12 18:31:57 +0100

    Extracted split method.

--- src/WordWrap.php
+++ src/WordWrap.php
@@ -4,32 +4,41 @@ namespace Mihaeu\Kata;
 
 class WordWrap
 {
+    /** @var string[] $lines */
+    private $lines = [];
+
+    private $currentLine = 0;
+
     public function wrap(\string $source, \int $columns) : \string
     {
-        /** @var string[] $lines */
-        $lines = [];
-        $currentLine = 0;
-        $lines[$currentLine] = '';
+        $this->lines = [];
+        $this->lines[$this->currentLine] = '';
 
+        $words = $this->splitWords($source);
+        foreach ($words as $word) {
+            if (preg_match('/\s/', $word) === 1) {
+                ++$this->currentLine;
+                $this->lines[$this->currentLine] = '';
+            }
+            else if (strlen($this->lines[$this->currentLine]) === 0) {
+                $this->lines[$this->currentLine] = $word;
+            } else if ((strlen($this->lines[$this->currentLine]) + strlen(' '.$word)) > $columns) {
+                $this->lines[++$this->currentLine] = $word;
+            } else {
+                $this->lines[$this->currentLine] .= ' '.$word;
+            }
+        }
+        return implode(PHP_EOL, $this->lines);
+    }
+
+    private function splitWords(\string $source) : array
+    {
         $wordsAndDelimiters = preg_split('/(\s)/', $source, -1, PREG_SPLIT_DELIM_CAPTURE);
         $words = array_filter($wordsAndDelimiters, function ($element) {
             if ($element != ' ') {
                 return $element;
             }
         });
-        foreach ($words as $word) {
-            if (preg_match('/\s/', $word) === 1) {
-                ++$currentLine;
-                $lines[$currentLine] = '';
-            }
-            else if (strlen($lines[$currentLine]) === 0) {
-                $lines[$currentLine] = $word;
-            } else if ((strlen($lines[$currentLine]) + strlen(' '.$word)) > $columns) {
-                $lines[++$currentLine] = $word;
-            } else {
-                $lines[$currentLine] .= ' '.$word;
-            }
-        }
-        return implode(PHP_EOL, $lines);
+        return $words;
     }
 }
